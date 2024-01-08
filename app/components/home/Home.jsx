3244b4a70f19835363043fd6320d437368dc2dda
Franco Ortega <francopdx@gmail.com>
2024-01-08 14:12:19 -0800

    create Home skeleton

---
+++ app/components/home/Home.jsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <main>
+      <section>
+        <h2>Section #1</h2>
+      </section>
+      <section>
+        <h2>Section #2</h2>
+      </section>
+      <section>
+        <h2>Section #3</h2>
+      </section>
+    </main>
+  );
+}
