894c4c90da80d7c3a6d7de15df50ce0a1cfbcbf9
Franco Ortega <francopdx@gmail.com>
2024-01-09 18:52:55 -0800

    change global bkgd to light black

--- components/home/Home.jsx
+++ components/home/Home.jsx
@@ -1,6 +1,8 @@
+import styles from './Home.module.scss';
+
 export default function Home() {
   return (
-    <main>
+    <main className={styles.Home}>
       <h2>Home Page</h2>
       <section>
         <h2>Section #1</h2>
