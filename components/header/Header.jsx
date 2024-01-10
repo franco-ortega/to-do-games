e14833b8c0e348fdb26dbca1b323bbae2ce2337b
Franco Ortega <francopdx@gmail.com>
2024-01-09 18:41:58 -0800

    add testing border to Header

--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -1,8 +1,9 @@
 import Nav from '../nav/Nav';
+import styles from './Header.module.scss';
 
 export default function Header() {
   return (
-    <header>
+    <header className={styles.Header}>
       <h1>Games To Play...</h1>
       <Nav />
     </header>
