dcb928f591adccbecc3cdd85a1fd9fbe85ae1844
Franco Ortega <francopdx@gmail.com>
2024-01-11 15:20:01 -0800

    pass week data as props into GamesToPlay and display list

--- app/week/page.tsx
+++ app/week/page.tsx
@@ -1,23 +1,40 @@
 'use client';
 
+import GamesToPlay from '@/components/gamesToPlay/GamesToPlay';
 import sampleData from '../../data/sample.json';
+import { useEffect, useState } from 'react';
 
 type Game = [string, { played: boolean }];
 
 export default function WeekPage() {
   const weekData = sampleData.week as Game[];
 
-  const gamesList: JSX.Element[] = weekData.map((game: Game) => (
-    <div key={game[0]}>
-      {game[0]} (played:{' '}
-      {game[1]['played'] ? <span>yes</span> : <span>no</span>})
-    </div>
-  ));
+  const [data, setData] = useState([] as Game[]);
+
+  console.log('DATA:', data);
+
+  useEffect(() => {
+    if (!data.length) setData(weekData);
+  }, [data, weekData]);
+
+  // const gamesList: JSX.Element[] = weekData.map((game: Game) => (
+  //   <div key={game[0]}>
+  //     {game[0]} (played:{' '}
+  //     {game[1]['played'] ? <span>yes</span> : <span>no</span>})
+  //   </div>
+  // ));
+
+  console.log('WEEK', weekData);
 
   return (
     <main>
-      <h2>Games to Play This Week</h2>
-      {gamesList}
+      <section>
+        <h2>Games to Play This Week</h2>
+        {/* {gamesList} */}
+      </section>
+      <section>
+        <GamesToPlay gameData={weekData} />
+      </section>
     </main>
   );
 }
--- components/gamesToPlay/GamesToPlay.tsx
+++ components/gamesToPlay/GamesToPlay.tsx
@@ -1,7 +1,36 @@
-export default function GamesToPlay() {
+'use client';
+
+import { useEffect, useState } from 'react';
+
+type Game = [string, { played: boolean }];
+
+type Props = { gameData: [string, { played: boolean }][] };
+
+const GamesToPlay = (gameData: Props): JSX.Element => {
+  console.log('GAME DATA:', gameData);
+  const [games, setGames] = useState([] as Game[]);
+
+  useEffect(() => {
+    if (!games.length) setGames(gameData.gameData);
+  }, [gameData, games]);
+
+  // console.log('GAMES:', games.gameData);
+
+  const gamesList: JSX.Element[] = games.map((game: Game) => (
+    <div key={game[0]}>
+      {game[0]} (played:{' '}
+      {game[1]['played'] ? <span>yes</span> : <span>no</span>})
+    </div>
+  ));
+
+  console.log('GAMES LIST', gamesList);
+
   return (
     <main>
       <h2>Games To Play</h2>
+      {gamesList}
     </main>
   );
-}
+};
+
+export default GamesToPlay;
