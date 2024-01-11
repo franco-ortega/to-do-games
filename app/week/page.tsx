e0cc9cb6f8632bd89e0f8e5700e6217f363a9230
Franco Ortega <francopdx@gmail.com>
2024-01-10 16:22:26 -0800

    move data into const

--- app/week/page.tsx
+++ app/week/page.tsx
@@ -6,15 +6,19 @@ export default function WeekPage() {
   const weekData = sampleData.week;
   console.log('Week DATA', weekData);
 
+  const gamesList: JSX.Element[] = weekData.map(
+    (game: [string, { played: boolean }][]) => (
+      <div key={game[0]}>
+        {game[0]} (played:{' '}
+        {game[0]['played'] ? <span>yes</span> : <span>no</span>})
+      </div>
+    )
+  );
+
   return (
     <main>
       <h2>Games to Play This Week</h2>
-      {weekData.map((game: [string, { played: boolean }][]) => (
-        <div key={game[0]}>
-          {game[0]} (played:{' '}
-          {game[0]['played'] ? <span>yes</span> : <span>no</span>})
-        </div>
-      ))}
+      {gamesList}
     </main>
   );
 }
