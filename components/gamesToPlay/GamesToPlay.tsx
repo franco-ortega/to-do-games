7e52c1032f811dead73a4578ff00c3bc858545fa
Franco Ortega <francopdx@gmail.com>
2024-01-10 16:25:18 -0800

    reset GamesToPlay to simple func

--- components/gamesToPlay/GamesToPlay.tsx
+++ components/gamesToPlay/GamesToPlay.tsx
@@ -1,34 +1,7 @@
-// type Game = [
-//   string,
-//   {
-//     played: boolean;
-//   }
-// ];
-
-// type Props = {
-//   games: Game[][];
-// };
-
-const GamesToPlay: React.FC<{
-  games: [][];
-}> = ({ games }) => {
-  console.log('GAMES TO PLAY', games);
-
+export default function GamesToPlay() {
   return (
     <main>
       <h2>Games To Play</h2>
     </main>
   );
-};
-
-export default GamesToPlay;
-
-// export default function GamesToPlay(games): React.FC<Props> {
-//   console.log('GAMES TO PLAY', games);
-
-//   return (
-//     <main>
-//       <h2>Games To Play</h2>
-//     </main>
-//   );
-// }
+}
