a13d3e538468aba863723ec0c0cc6bebf8788b62
Franco Ortega <francopdx@gmail.com>
2024-01-11 15:51:59 -0800

    remove state from Week page

--- app/week/page.tsx
+++ app/week/page.tsx
@@ -2,20 +2,11 @@
 
 import GamesToPlay from '@/components/gamesToPlay/GamesToPlay';
 import sampleData from '../../data/sample.json';
-import { useEffect, useState } from 'react';
 
 type Game = [string, { played: boolean }];
 
 export default function WeekPage() {
   const weekData = sampleData.week as Game[];
-
-  const [data, setData] = useState([] as Game[]);
-
-  console.log('DATA:', data);
-
-  useEffect(() => {
-    if (!data.length) setData(weekData);
-  }, [data, weekData]);
 
   return <GamesToPlay gameData={weekData} time={'Week'} />;
 }
