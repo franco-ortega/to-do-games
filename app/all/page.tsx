e348f1a9558ef11f34a247adf8b6ac14eb0b74fd
Franco Ortega <francopdx@gmail.com>
2024-01-12 17:07:06 -0800

    create page to display all games to play

---
+++ app/all/page.tsx
@@ -0,0 +1,15 @@
+import GamesToPlay from '@/components/gamesToPlay/GamesToPlay';
+import gameData from '../../data/sample.json';
+import { Game } from '@/utils/types';
+import { timeSpan } from '@/utils/enums';
+
+export default function AllPage() {
+  return (
+    <main>
+      <h2>All Games to Play</h2>
+      <GamesToPlay gameData={gameData.week as Game[]} time={timeSpan.week} />
+      <GamesToPlay gameData={gameData.month as Game[]} time={timeSpan.month} />
+      <GamesToPlay gameData={gameData.year as Game[]} time={timeSpan.year} />
+    </main>
+  );
+}
