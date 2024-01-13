e3f9eb8edcc55423dcc59f5a78235de5dc08d1d7
Franco Ortega <francopdx@gmail.com>
2024-01-12 17:25:32 -0800

    destructure gameList items for clarity

--- components/gamesList/GamesList.tsx
+++ components/gamesList/GamesList.tsx
@@ -7,10 +7,9 @@ type Props = {
 export default function GamesList({ gamesList }: Props) {
   return (
     <ul>
-      {gamesList.map((game: Game) => (
-        <li key={game[0]}>
-          {game[0]} (played:{' '}
-          {game[1]['played'] ? <span>yes</span> : <span>no</span>})
+      {gamesList.map(([game, { played }]) => (
+        <li key={game}>
+          {game} (played: {played ? <span>yes</span> : <span>no</span>})
         </li>
       ))}
     </ul>
