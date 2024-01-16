0dafff7a099a64fb4c673274d4da8e3dabd998ba
Franco Ortega <francopdx@gmail.com>
2024-01-15 22:45:13 -0800

    create form to add new games

--- components/home/Home.tsx
+++ components/home/Home.tsx
@@ -1,12 +1,37 @@
+'use client';
+
+import { useState } from 'react';
+import { Game } from '../../utils/types';
 import styles from './Home.module.scss';
 
 export default function Home() {
+  const [game, setGame] = useState('');
+  const [games, setGames] = useState([] as Game[]);
+
+  const addGame: React.FormEventHandler = (
+    e: React.FormEvent<HTMLInputElement>
+  ) => {
+    e.preventDefault();
+    setGames((prevState) => [...prevState, [game, { isPlayed: false }]]);
+  };
+
   return (
     <main className={styles.Home}>
       <h2>Home Page</h2>
       <section>
         The links for This Week, This Month, and This Year now dynamically
         display sample data.
+      </section>
+      <section>
+        <form onSubmit={addGame}>
+          <p>
+            <label>
+              Game:{' '}
+              <input type='text' onChange={(e) => setGame(e.target.value)} />
+            </label>
+          </p>
+          <button>Add Game</button>
+        </form>
       </section>
     </main>
   );
