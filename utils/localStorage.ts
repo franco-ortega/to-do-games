48591fa337da5797d023dc2c03c40723de49e4e3
Franco Ortega <francopdx@gmail.com>
2024-01-24 23:51:44 -0800

    check to see if window is undefined

--- utils/localStorage.ts
+++ utils/localStorage.ts
@@ -1,16 +1,22 @@
 import { GamesToPlay } from "./types";
 
 export function setGames(key: string, value: GamesToPlay) {
-  const stringyGames = JSON.stringify(value);
-    window.localStorage.setItem(key, stringyGames);
+
+  if(typeof window !== 'undefined') {
+    const stringyGames = JSON.stringify(value);
+      window.localStorage.setItem(key, stringyGames);
+
+  }
 };
 
 export function getGames(key: string) {
-  const stringyGames = window.localStorage.getItem(key);
+  if(typeof window !== 'undefined') {
+    const stringyGames = window.localStorage.getItem(key);
 
-  if(stringyGames) {
-    const parsedGames: GamesToPlay = JSON.parse(stringyGames);
-    return parsedGames
+    if(stringyGames) {
+      const parsedGames: GamesToPlay = JSON.parse(stringyGames);
+      return parsedGames
+    }
   }
 
   return {} as GamesToPlay;
