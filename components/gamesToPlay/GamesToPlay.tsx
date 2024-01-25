4da0ad716338972d0fab5e6b7de9584cce1d4996
Franco Ortega <francopdx@gmail.com>
2024-01-25 00:02:09 -0800

    call localStorage in useEffect

--- components/gamesToPlay/GamesToPlay.tsx
+++ components/gamesToPlay/GamesToPlay.tsx
@@ -1,4 +1,7 @@
-import { GamesToPlay, TimeSpanOptions } from '../../utils/types';
+'use client';
+
+import { useEffect, useState } from 'react';
+import { Game, GamesToPlay, TimeSpanOptions } from '../../utils/types';
 import GamesList from '../gamesList/GamesList';
 import { getGames } from '@/utils/localStorage';
 
@@ -7,14 +10,19 @@ type Props = {
 };
 
 export default function GamesToPlay({ timeSpan }: Props): JSX.Element {
-  const currentGames = getGames('GAMES_TO_PLAY');
-  const time = timeSpan.toLowerCase() as TimeSpanOptions;
-  const gameInfo = currentGames[time] ? currentGames[time] : [];
+  const [gameStuff, setGameStuff] = useState([] as Game[]);
+
+  useEffect(() => {
+    const currentGames = getGames('GAMES_TO_PLAY');
+    const time = timeSpan.toLowerCase() as TimeSpanOptions;
+    const gameInfo = currentGames[time] ? currentGames[time] : [];
+    setGameStuff(gameInfo);
+  }, [timeSpan]);
 
   return (
     <main>
       <h2>Games To Play This {timeSpan}</h2>
-      <GamesList gamesList={gameInfo} />
+      <GamesList gamesList={gameStuff} />
     </main>
   );
 }
