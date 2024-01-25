d7e996f41a914b1d00fa5f80e197e62585e4a4c5
Franco Ortega <francopdx@gmail.com>
2024-01-24 23:41:44 -0800

    update all page to use data from local storage

--- app/all/page.tsx
+++ app/all/page.tsx
@@ -1,24 +1,16 @@
+'use client';
+
 import GamesToPlay from '@/components/gamesToPlay/GamesToPlay';
-import gameData from '../../data/sample.json';
-import { Game } from '@/utils/types';
+import { TimeSpanOptions } from '@/utils/types';
 import { timeSpan } from '@/utils/enums';
 
 export default function AllPage() {
   return (
     <main>
       <h2>All Games to Play</h2>
-      <GamesToPlay
-        gameData={gameData.week as Game[]}
-        timeSpan={timeSpan.week}
-      />
-      <GamesToPlay
-        gameData={gameData.month as Game[]}
-        timeSpan={timeSpan.month}
-      />
-      <GamesToPlay
-        gameData={gameData.year as Game[]}
-        timeSpan={timeSpan.year}
-      />
+      <GamesToPlay timeSpan={timeSpan.week as TimeSpanOptions} />
+      <GamesToPlay timeSpan={timeSpan.month as TimeSpanOptions} />
+      <GamesToPlay timeSpan={timeSpan.year as TimeSpanOptions} />
     </main>
   );
 }
