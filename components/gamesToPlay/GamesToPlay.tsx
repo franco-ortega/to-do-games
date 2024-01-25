1f74451abdda4c5c84396e8e87961b180c758f70
Franco Ortega <francopdx@gmail.com>
2024-01-24 23:28:06 -0800

    display data from local storage

--- components/gamesToPlay/GamesToPlay.tsx
+++ components/gamesToPlay/GamesToPlay.tsx
@@ -1,21 +1,25 @@
 import { useEffect, useState } from 'react';
 
-import { Game } from '../../utils/types';
+import { Game, GamesToPlay, TimeSpanOptions } from '../../utils/types';
 import GamesList from '../gamesList/GamesList';
+import { getGames } from '@/utils/localStorage';
 
 type Props = {
   gameData: Game[];
-  timeSpan: string;
+  timeSpan: TimeSpanOptions;
 };
 
 export default function GamesToPlay({
   gameData,
   timeSpan,
 }: Props): JSX.Element {
+  const currentGames = getGames('GAMES_TO_PLAY');
+  const time = timeSpan.toLowerCase() as TimeSpanOptions;
+
   return (
     <main>
       <h2>Games To Play This {timeSpan}</h2>
-      <GamesList gamesList={gameData} />
+      <GamesList gamesList={currentGames[time]} />
     </main>
   );
 }
