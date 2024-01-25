2d23ba41d8a2445ae64b1581468b0cb36136eaea
Franco Ortega <francopdx@gmail.com>
2024-01-24 23:07:15 -0800

    create getGames to "type safely" parse data from local storage

--- utils/localStorage.ts
+++ utils/localStorage.ts
@@ -3,4 +3,13 @@ import { GamesToPlay } from "./types";
 export function setGames(key: string, value: GamesToPlay) {
   const stringyGames = JSON.stringify(value);
     localStorage.setItem(key, stringyGames);
-}
+};
+
+export function getGames(key: string) {
+  const stringyGames = localStorage.getItem(key);
+
+  if(stringyGames) {
+    const parsedGames: GamesToPlay = JSON.parse(stringyGames);
+    return parsedGames
+  }
+};
