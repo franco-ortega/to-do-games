ec0ae25e43721c85b3d94125e466829868377e0d
Franco Ortega <francopdx@gmail.com>
2024-01-29 22:53:25 -0800

    return empty but structured object if it does not exist yet from local storage

--- utils/localStorage.ts
+++ utils/localStorage.ts
@@ -1,17 +1,22 @@
 import { GamesToPlay } from "./types";
 
 export function setGames(key: string, value: GamesToPlay) {
-    const stringyGames = JSON.stringify(value);
-      window.localStorage.setItem(key, stringyGames);
+  const stringyGames = JSON.stringify(value);
+  window.localStorage.setItem(key, stringyGames);  
 };
 
 export function getGames(key: string) {
-    const stringyGames = window.localStorage.getItem(key);
+  const stringyGames = window.localStorage.getItem(key);
 
-    if(stringyGames) {
-      const parsedGames: GamesToPlay = JSON.parse(stringyGames);
-      return parsedGames
-    }
+  if(stringyGames) {
+    const parsedGames: GamesToPlay = JSON.parse(stringyGames);
+    return parsedGames
+  }
 
-  return {} as GamesToPlay;
+  return {
+    week: [],
+    month: [],
+    year: []
+    
+  } as GamesToPlay;
 };
