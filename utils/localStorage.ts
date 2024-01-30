df22fe47524ed511b5144d6d9aa0d815560d21b6
Franco Ortega <francopdx@gmail.com>
2024-01-29 22:37:57 -0800

    remove unused commented out code

--- utils/localStorage.ts
+++ utils/localStorage.ts
@@ -1,22 +1,17 @@
 import { GamesToPlay } from "./types";
 
 export function setGames(key: string, value: GamesToPlay) {
-
-  // if(typeof window !== 'undefined') {
     const stringyGames = JSON.stringify(value);
       window.localStorage.setItem(key, stringyGames);
-  // }
 };
 
 export function getGames(key: string) {
-  // if(typeof window !== 'undefined') {
     const stringyGames = window.localStorage.getItem(key);
 
     if(stringyGames) {
       const parsedGames: GamesToPlay = JSON.parse(stringyGames);
       return parsedGames
     }
-  // }
 
   return {} as GamesToPlay;
 };
