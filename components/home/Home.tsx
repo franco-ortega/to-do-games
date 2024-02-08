c758aea9fca3e4f2232a9ba696c0b20b16b6535c
Franco Ortega <francopdx@gmail.com>
2024-02-07 21:40:25 -0500

    remove default header from Home

--- components/home/Home.tsx
+++ components/home/Home.tsx
@@ -7,7 +7,6 @@ export default function Home() {
   return (
     <main className={styles.Home}>
       <section>
-        <h2>Home Page</h2>
         <p>
           Make lists of the games that you want to play this week, this month,
           and this year.
