c39bb4609fedb76445f143c23faab38c3058e1e2
Franco Ortega <francopdx@gmail.com>
2024-02-15 23:45:03 -0800

    remove sample data imports and uses

--- app/week/page.tsx
+++ app/week/page.tsx
@@ -1,12 +1,9 @@
 'use client';
 
 import GamesToPlay from '@/components/gamesToPlay/GamesToPlay';
-import sampleData from '../../data/sample.json';
-import { Game, TimeSpanOptions } from '../../utils/types';
+import { TimeSpanOptions } from '../../utils/types';
 import { timeSpan } from '@/utils/enums';
 
 export default function WeekPage() {
-  const weekData = sampleData.week as Game[];
-
   return <GamesToPlay timeSpan={timeSpan.week as TimeSpanOptions} />;
 }
--- app/year/page.tsx
+++ app/year/page.tsx
@@ -1,12 +1,9 @@
 'use client';
 
 import GamesToPlay from '@/components/gamesToPlay/GamesToPlay';
-import sampleData from '../../data/sample.json';
 import { Game, TimeSpanOptions } from '@/utils/types';
 import { timeSpan } from '@/utils/enums';
 
 export default function YearPage() {
-  const data = sampleData.year as Game[];
-
   return <GamesToPlay timeSpan={timeSpan.year as TimeSpanOptions} />;
 }
