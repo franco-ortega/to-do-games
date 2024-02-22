c98d797bb406cb1f2ccaecdac0f9353a040df2b1
Franco Ortega <francopdx@gmail.com>
2024-02-21 16:57:52 -0800

    update all page to use timeSpanOptions enum

--- app/all/page.tsx
+++ app/all/page.tsx
@@ -1,16 +1,15 @@
 'use client';
 
 import GamesToPlay from '@/components/gamesToPlay/GamesToPlay';
-import { TimeSpanOptions } from '@/utils/types';
-import { timeSpan } from '@/utils/enums';
+import { timeSpanOptions } from '@/utils/enums';
 
 export default function AllPage() {
   return (
     <main>
       <h2>All Games to Play</h2>
-      <GamesToPlay timeSpan={timeSpan.week.title as TimeSpanOptions} />
-      <GamesToPlay timeSpan={timeSpan.month.title as TimeSpanOptions} />
-      <GamesToPlay timeSpan={timeSpan.year.title as TimeSpanOptions} />
+      <GamesToPlay timeSpan={timeSpanOptions.week} />
+      <GamesToPlay timeSpan={timeSpanOptions.month} />
+      <GamesToPlay timeSpan={timeSpanOptions.year} />
     </main>
   );
 }
