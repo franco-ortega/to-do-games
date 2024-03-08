acaef197dd2fc2a9168c9981c3d21fbb994e5953
Franco Ortega <francopdx@gmail.com>
2024-03-07 16:45:07 -0800

    remove unnecessary props

--- components/allGames/AllGames.tsx
+++ components/allGames/AllGames.tsx
@@ -2,9 +2,7 @@ import GamesToPlay from '../gamesToPlay/GamesToPlay';
 import { timeSpanOptions } from '@/utils/enums';
 import styles from './AllGames.module.scss';
 
-type Props = {};
-
-export default function AllGames({}: Props) {
+export default function AllGames() {
   return (
     <main className={styles.AllGames}>
       <h2>All Games to Play</h2>
