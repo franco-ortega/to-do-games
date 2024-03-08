7cbb7543e1f8cf8096fdc0e20d41a34fefc30aaa
Franco Ortega <francopdx@gmail.com>
2024-03-07 16:36:15 -0800

    display game lists in a row on wider screen sizes

--- components/allGames/AllGames.tsx
+++ components/allGames/AllGames.tsx
@@ -1,16 +1,18 @@
-import React from 'react';
 import GamesToPlay from '../gamesToPlay/GamesToPlay';
 import { timeSpanOptions } from '@/utils/enums';
+import styles from './AllGames.module.scss';
 
 type Props = {};
 
 export default function AllGames({}: Props) {
   return (
-    <main>
+    <main className={styles.AllGames}>
       <h2>All Games to Play</h2>
-      <GamesToPlay timeSpan={timeSpanOptions.week} />
-      <GamesToPlay timeSpan={timeSpanOptions.month} />
-      <GamesToPlay timeSpan={timeSpanOptions.year} />
+      <section>
+        <GamesToPlay timeSpan={timeSpanOptions.week} />
+        <GamesToPlay timeSpan={timeSpanOptions.month} />
+        <GamesToPlay timeSpan={timeSpanOptions.year} />
+      </section>
     </main>
   );
 }
