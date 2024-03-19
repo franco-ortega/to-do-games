c3972a83898c661e9565668fb524e3e6a5ce14c7
Franco Ortega <francopdx@gmail.com>
2024-03-19 16:07:46 -0700

    remove header types

--- utils/types.ts
+++ utils/types.ts
@@ -14,9 +14,7 @@ export type GamesToPlay = {
 };
 
 export type TimeSpanPaths = keyof GamesToPlay;
-export type TimeSpanHeaders = 'Week' | 'Month' | 'Year';
 
 export type TimeSpan = {
   path: TimeSpanPaths;
-  header: TimeSpanHeaders;
 }
