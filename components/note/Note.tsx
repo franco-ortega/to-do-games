e581fb6c5d06a55e277327ef56b8a88a3d0c5d3a
Franco Ortega <francopdx@gmail.com>
2024-03-26 18:08:46 -0700

    increase specificity for button styles across components

--- components/note/Note.tsx
+++ components/note/Note.tsx
@@ -18,6 +18,8 @@ export default function Note({
 }: Props) {
   const [isEditNote, setIsEditNote] = useState(false);
 
+  console.log(isEditNote);
+
   const toggleEditNote = () => {
     setIsEditNote((prev) => !prev);
   };
