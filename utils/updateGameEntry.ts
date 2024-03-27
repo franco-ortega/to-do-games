b8b1996ea67357d816e9feb857a5b9c41e790611
Franco Ortega <francopdx@gmail.com>
2024-03-27 14:54:25 -0700

    update updateGameEntry to return matching data structure for Game

--- utils/updateGameEntry.ts
+++ utils/updateGameEntry.ts
@@ -1,25 +1,31 @@
 import { getGames, setGames } from "./localStorage";
 import { Game, GamesToPlay, TimeSpanPaths } from "./types";
 
-export default function updateGameEntry(pathname: TimeSpanPaths, game: Game) {
+export default function updateGameEntry(pathname: TimeSpanPaths, gameToUpdate: Game) {
 
   const savedGames = getGames('GAMES_TO_PLAY') as GamesToPlay;
 
   const updatedGames = {
     ...savedGames,
-    [pathname]: savedGames[pathname].map((gameItem) => {
+    [pathname]: savedGames[pathname].map((game) => {
       const title = Array.isArray(game) ? game[0] : game.title;
       const isPlayed = Array.isArray(game) ? game[1].isPlayed : game.isPlayed;
       const note = Array.isArray(game) ? game[1].note : game.note;
 
-      if (Array.isArray(game) ? game[0] : game.title === (title)) {
-        return {
-            title,
+      if (Array.isArray(gameToUpdate) ? gameToUpdate[0] : gameToUpdate.title === (title)) {
+        return Array.isArray(gameToUpdate) ? [
+          title,
+          {
             isPlayed: !isPlayed,
             note
           }
-        ;
-      } else return gameItem;
+        ] : 
+        {
+          title,
+          isPlayed: !isPlayed,
+          note
+        };
+      } else return game;
     }),
   };
 
