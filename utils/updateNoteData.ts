01f31be8998f45f654b5998476736a670348e8a7
Franco Ortega <francopdx@gmail.com>
2024-03-28 21:53:46 -0700

    deprecate updateNoteData

--- utils/updateNoteData.ts
+++ utils/updateNoteData.ts
@@ -1,29 +1,29 @@
-import { getGames, setGames } from "./localStorage";
-import { TimeSpanPaths } from "./types";
+// import { getGames, setGames } from "./localStorage";
+// import { TimeSpanPaths } from "./types";
 
-export default function updateNoteData() {
+// export default function updateNoteData() {
 
-  const savedGames = getGames('GAMES_TO_PLAY');
+//   const savedGames = getGames('GAMES_TO_PLAY');
   
-  for (const timeSpan in savedGames) {
-    const gamesPerTimeSpan = savedGames[timeSpan as TimeSpanPaths]
+//   for (const timeSpan in savedGames) {
+//     const gamesPerTimeSpan = savedGames[timeSpan as TimeSpanPaths]
 
-    if(gamesPerTimeSpan.length) {
-      gamesPerTimeSpan.forEach(game => {
-        const gameData = Array.isArray(game) ? game[1] : game;
+//     if(gamesPerTimeSpan.length) {
+//       gamesPerTimeSpan.forEach(game => {
+//         const gameData = Array.isArray(game) ? game[1] : game;
 
-        // this checks to see if "notes" exists
-        if (Object.keys(gameData).includes('notes')) {
+//         // this checks to see if "notes" exists
+//         if (Object.keys(gameData).includes('notes')) {
 
-          // first - add the "note" variable
-          gameData.note = Array.isArray(game) ? game[1].notes : game.note; 
+//           // first - add the "note" variable
+//           gameData.note = Array.isArray(game) ? game[1].notes : game.note; 
           
-          // second - remove the "notes" variable
-          Array.isArray(game) && delete game[1].notes 
-        }
-      })
-    }
-  }
+//           // second - remove the "notes" variable
+//           Array.isArray(game) && delete game[1].notes 
+//         }
+//       })
+//     }
+//   }
 
-  setGames('GAMES_TO_PLAY', savedGames)
-};
+//   setGames('GAMES_TO_PLAY', savedGames)
+// };
