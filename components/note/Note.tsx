6b5f2c5327b9b60a15488a10ae81976d0dab6701
Franco Ortega <francopdx@gmail.com>
2024-03-30 17:16:49 -0700

    remove Note title and EditNoteBtn from Note

--- components/note/Note.tsx
+++ components/note/Note.tsx
@@ -9,10 +9,6 @@ type Props = {
 export default function Note({ note, toggleEditNote }: Props) {
   return (
     <div className={styles.Note}>
-      <div>
-        <h4>Note</h4>
-        <EditNoteBtn toggle={toggleEditNote} />
-      </div>
       <p>{note}</p>
     </div>
   );
